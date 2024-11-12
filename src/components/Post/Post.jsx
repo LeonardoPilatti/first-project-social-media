2e1c14bd5a8605bee9736916354d7e19694001d4
LeonardoPilatti <leonardopilatti14@gmail.com>
2024-11-11 23:03:55 -0300

    feat: Add commentForm

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -30,6 +30,17 @@ export const Post = () => {
                 <a href="#">#rocketseat</a>
             </p>
             </div>
+
+            <form className={S.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'    
+                />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
